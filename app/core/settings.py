5f83fe7fbe803b11b885cf0baa7db8813f34cdd4
TeaDove <peter.ibragimov@gmail.com>
2021-07-10 01:20:24 +0300

    Auto stash before merge of "master" and "origin/master"

--- app/core/settings.py
+++ app/core/settings.py
@@ -3,7 +3,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     telegram_api: str
-    sqlite_dsn: str = "sqlite:///app/db/data/main.sqlite3"
+    sqlite_dsn: str = "sqlite:///app/db_data/main.sqlite3"
 
     class Config:
         case_sensitive = False
